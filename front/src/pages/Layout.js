b79ca165a6a4c04c9afdeaadeaebd1de9ef6385b
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-12-11 12:25:11 +0100

    add form contact add routeur

---
+++ front/src/pages/Layout.js
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+;
+
+const Layout = () => {
+  return (
+    <>
+
+      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+          <Link className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+                to="/">
+            Home
+          </Link>
+        <div>
+          <Link to="/" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Home</Link>
+          <Link to="/contact" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Contact</Link>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
---
+++ front/src/pages/NoPage.js
@@ -0,0 +1,7 @@
+export default function NoPage() {
+  return (
+    <h1 className="text-3xl text-center font-bold">
+      NoPage
+    </h1>
+  )
+}
