c7d14fc7cf6e8ac7d9ac649134921be87d302b9a
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-12-11 14:58:04 +0100

    edit form

--- front/src/pages/Contact.js
+++ front/src/pages/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
         body: JSON.stringify({
           nom: nom,
           prenom: prenom,
-          mail: mail,
+          email: mail,
           message: message,
        })
     }).then(data => data.json())
@@ -56,29 +56,29 @@ export default function Contact() {
   }
 
   return (
-    <section class="bg-white dark:bg-gray-900">
+    <section class="bg-white">
       <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contacter Nous</h2>
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contacter Nous</h2>
           <form action="#" class="space-y-8" onSubmit={handleSubmit}>
               <div>
-                  <label for="nom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nom</label>
-                  <input type="text" id="nom" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                  <label for="nom" class="block mb-2 text-sm font-medium text-gray-900">Nom</label>
+                  <input type="text" id="nom" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
                           placeholder="Nom" required
                           onChange={handleChangeNom} value={nom}
                   >
                   </input>
               </div>
               <div>
-                  <label for="prenom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Prenom</label>
-                  <input type="text" id="prenom" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                  <label for="prenom" class="block mb-2 text-sm font-medium text-gray-900">Prenom</label>
+                  <input type="text" id="prenom" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
                           placeholder="Prenom" required
                           onChange={handleChangePrenom} value={prenom}
                   >
                   </input>
               </div>
               <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                  <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                  <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
                           placeholder="name@flowbite.com" required
                           onChange={handleChangeMail} value={mail}
                   >
@@ -86,13 +86,15 @@ export default function Contact() {
               </div>
 
               <div class="sm:col-span-2">
-                  <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Message</label>
-                  <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                  <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Message</label>
+                  <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" 
                             placeholder="Laisser nous un commentaire..."
                             onChange={handleChangeMessage} value={message}
                   ></textarea>
               </div>
-              <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Envoyer</button>
+              <button type="submit" class="py-3 px-5 text-sm font-medium text-center  text-gray-900 rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
+                Envoyer
+              </button>
           </form>
       </div>
     </section>
