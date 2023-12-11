2d71ff619af18664227ff59d5c0c774f2f80665b
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-12-11 11:53:34 +0100

    connexion db and cors
    connexion db and cors

--- back/main.js
+++ back/main.js
@@ -1,12 +1,39 @@
 // index.js
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
 // Middleware body-parser pour traiter les données JSON
 app.use(bodyParser.json());
+
+// Middleware CORS pour autoriser toutes les origines, méthodes et en-têtes
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
+
+// Connexion à la base de données MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/account-projet4-express", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB database"))
+  .catch((error) =>
+    console.log("Error connecting to MongoDB database: ", error)
+  );
+
+
+app.listen(port, () => {
+  console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+});
 
 // Route POST pour traiter les données JSON
 app.post("/api/data", (req, res) => {
@@ -15,6 +42,3 @@ app.post("/api/data", (req, res) => {
   res.json({ message: "Données reçues avec succès" });
 });
 
-app.listen(port, () => {
-  console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
-});
