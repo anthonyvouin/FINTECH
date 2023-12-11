dadd720f6a1029a215cfd382c47f09888e6afee6
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-12-11 17:16:14 +0100

    update
    update

--- front/src/pages/Layout.js
+++ front/src/pages/Layout.js
@@ -3,20 +3,31 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-
       <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
-          <Link className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
-                to="/">
+        <Link
+          className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+          to="/"
+        >
+          TradeMinds
+        </Link>
+        <div>
+          <Link
+            to="/"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
             Home
           </Link>
-        <div>
-          <Link to="/" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Home</Link>
-          <Link to="/contact" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Contact</Link>
+          <Link
+            to="/contact"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
+            Contact
+          </Link>
         </div>
       </nav>
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
