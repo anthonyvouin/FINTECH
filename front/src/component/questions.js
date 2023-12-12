5adfbc7343d9c8ec61868f9f7e370f2d045c9f6a
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-12-12 11:05:21 +0100

    update reponse response

--- front/src/component/questions.js
+++ front/src/component/questions.js
@@ -29,56 +29,77 @@ const Questions = (props) => {
                   { question: props.question.questions.q2, reponse:q2 }
                 ]}
             );
-            alert("merci pour vos réponses");
 
-            setQ1("");
-            setQ2("");
+            document.querySelector("#question_"+props.question.id).innerHTML = returnResponse();
+            localStorage.setItem("#question_"+props.question.id, true)
         } catch (error) {
             alert("petit problème durant l'envoie de vos réponses");
         }
     }
-    
+
+    function returnResponse() {
+        return `
+            <h1 class="max-w-2xl mb-4 text-xl font-extrabold leading-none ">${props.question.title}</h1>
+            <div>
+                <div>
+                    Vos réponses sont bien été envoyé
+                </div>
+                <span>✔</span>
+            </div>
+        `;
+    }
 
     return (
-    <div className={`grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 gap-2 xl:gap-0 lg:py-16 lg:grid-cols-12 ${
-        props.question.id % 2 !== 0 ? " lg:flex-row-reverse lg:flex-wrap" : ""
-        }`}>
-
-        <div className={`mr-auto w-full place-self-center lg:col-span-6 ${
-        props.question.id % 2 !== 0 ? " order-2" : ""
-        }`}>
-            <img src={ props.question.image } alt="" className="portfolio__img"/>
-        </div>  
-
-        <div className={`mr-auto w-full place-self-center lg:col-span-6 ${
-        props.question.id % 2 !== 0 ? " order-1" : ""
-        }`}>
-            <h1 className="max-w-2xl mb-4 text-xl font-extrabold leading-none ">{ props.question.title }</h1>
-            <form action="#" className="" onSubmit={handleSubmit}>
+        <div className={`grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 gap-2 xl:gap-0 lg:py-16 lg:grid-cols-12 ${props.question.id % 2 !== 0 ? "lg:flex-row-reverse lg:flex-wrap" : ""}`}>
+          <div className={`mr-auto w-full place-self-center lg:col-span-6 ${props.question.id % 2 !== 0 ? "lg:order-2" : ""}`}>
+            <img src={props.question.image} alt="" className="portfolio__img" />
+          </div>
+          <div id={"question_" + props.question.id} className={`lg:px-4 mr-auto w-full place-self-center lg:col-span-6 ${props.question.id % 2 !== 0 ? "lg:order-1" : ""}`}>
+            <h1 className="max-w-2xl mb-4 text-xl font-extrabold leading-none ">{props.question.title}</h1>
+            {localStorage.getItem("#question_" + props.question.id) !== "true" ? (
+              <form action="#" className="" onSubmit={handleSubmit}>
                 <div className="">
-                    <div className="mb-5">
-                        <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900">{ props.question.questions.q1 }</label>
-                        <input type="text" id="q1" placeholder="Reponse" onChange={handleChangeQ1} required value={q1}
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                        />
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="q2" className="block mb-2 text-sm font-medium text-gray-900">{ props.question.questions.q2 }</label>
-                        <input type="text" id="q2" placeholder="Reponse" onChange={handleChangeQ2} required value={q2}
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                        />
-                    </div>
+                  <div className="mb-5">
+                    <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900">{props.question.questions.q1}</label>
+                    <input
+                      type="text"
+                      id="q1"
+                      placeholder="Reponse"
+                      onChange={handleChangeQ1}
+                      required
+                      value={q1}
+                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <label htmlFor="q2" className="block mb-2 text-sm font-medium text-gray-900">{props.question.questions.q2}</label>
+                    <input
+                      type="text"
+                      id="q2"
+                      placeholder="Reponse"
+                      onChange={handleChangeQ2}
+                      required
+                      value={q2}
+                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                    />
+                  </div>
                 </div>
-
-                <button type="submit"
-                        className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      
+                <button
+                  type="submit"
+                  className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                    Envoyer
+                  Envoyer
                 </button>
-            </form>
-        </div>              
-    </div>
-    )
+              </form>
+            ) : (
+              <div>
+                Vos réponses ont bien été envoyées <span>✔</span>
+              </div>
+            )}
+          </div>
+        </div>
+      );      
 }
 
 export default Questions;
