aa767bc13f546917d924fd2d8b41a991d6ad4c06
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-12-13 17:58:18 +0100

    add dashboard admin
    add dashboard admin

--- front/src/pages/Admin.js
+++ front/src/pages/Admin.js
@@ -1,25 +1,42 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Admin() {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-  
-    fetch(`http://localhost:3000/api/questions-reponses`, {
-      method: 'GET',
-    }).then(data => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/questions-reponses");
+        const data = await response.json();
+        setQuestions(data.questionsReponses);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des questions et réponses :", error);
+        // Gérer l'erreur ici, par exemple afficher un message d'erreur à l'utilisateur.
+      }
+    };
 
-      console.log(data);
-      setQuestions(data.json().questions)
-    })
+    fetchQuestions();
   }, []);
 
-
   return (
-    <h1 className="text-3xl text-center font-bold">
-      azeryuihgfddfghj
-    </h1>
-  )
+    <div>
+      <h1 className="text-3xl text-center font-bold">azeryuihgfddfghj</h1>
+      <div>
+        {/* Afficher le contenu des questions ici */}
+        {questions.map((question, index) => (
+          <div key={index}>
+            <h2>{question._id}</h2>
+            <ul>
+              {question.reponses.map((reponse, subIndex) => (
+                <li key={subIndex}>
+                  <p>Question: {reponse.question}</p>
+                  <p>Réponse: {reponse.reponse}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- front/src/pages/NoPage.js
+++ front/src/pages/NoPage.js
@@ -1,7 +1,7 @@
 export default function NoPage() {
   return (
     <h1 className="text-3xl text-center font-bold">
-      NoPage
+      No Page
     </h1>
   )
 }
