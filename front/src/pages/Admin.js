b63bade7e312c171a852f47eb4632364a404c120
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-12-13 17:28:09 +0100

    somes edit

--- front/src/pages/Admin.js
+++ front/src/pages/Admin.js
@@ -9,34 +9,15 @@ export default function Admin() {
   
     fetch(`http://localhost:3000/admin/api/questions-reponses`, {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${username}:${password}`,
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password
-      }
-    )})
-    .then(data => {
-      if (!data.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      return data.json(); // Convertit les données de la réponse en JSON
+    }).then(data => {
+      setQuestions(data.json().questions)
     })
-    .then(parsedData => {
-      console.log(parsedData); // Affiche les données obtenues
-      setQuestions(parsedData); // Met à jour l'état avec les données récupérées
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
   }, []);
 
 
   return (
     <h1 className="text-3xl text-center font-bold">
-      NoPAdminage
+      azeryuihgfddfghj
     </h1>
   )
 }
