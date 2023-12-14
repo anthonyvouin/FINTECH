9c0f9bca638cbee7c6273f010393349b976c88be
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-12-14 14:49:02 +0100

    somes edit

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,57 +1,62 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { questions } from "../datas/questions";
 import hero_image from "../media/office-workers-using-finance-graphs_23-2150408681.jpg";
 
 export default function Home() {
   const ref = useRef(null);
-  const [responses, setResponses] = useState([]);
+  const [responses, setResponses] = useState(Array(questions.length).fill(''));
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [inputValue, setInputValue] = useState(responses[currentQuestionIndex]);
+  const [showError, setShowError] = useState(false);
   const [progress, setProgress] = useState(0);
 
   const handleClick = () => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    const questionId = questions[currentQuestionIndex].id;
-
-    const existingResponseIndex = responses.findIndex(
-      (response) => response.id === questionId
-    );
-
-    if (existingResponseIndex !== -1) {
-      const updatedResponses = [...responses];
-      updatedResponses[existingResponseIndex].response = value;
-      setResponses(updatedResponses);
-    } else {
-      setResponses([...responses, { id: questionId, response: value }]);
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+      const newProgress = ((currentQuestionIndex - 1) / questions.length) * 100;
+      setProgress(newProgress);
+      setInputValue(responses[currentQuestionIndex - 1]);
+      setShowError(false);
     }
   };
-
   const handleNextQuestion = () => {
-    if (currentQuestionIndex < questions.length - 1) {
+    if (inputValue.trim() !== '' && currentQuestionIndex < questions.length - 1) {
+      setShowError(false);
+      const updatedResponses = [...responses];
+      updatedResponses[currentQuestionIndex] = inputValue;
+      setResponses(updatedResponses);
       setCurrentQuestionIndex(currentQuestionIndex + 1);
       const newProgress = ((currentQuestionIndex + 1) / questions.length) * 100;
       setProgress(newProgress);
+      setInputValue(responses[currentQuestionIndex + 1]);
+    } else {
+      setShowError(true);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const questionsResponses = responses.map((response) => {
-      const question = questions.find((q) => q.id === response.id);
-      return { question: question.question, reponse: response.response };
+    const updatedResponses = [...responses];
+    updatedResponses[currentQuestionIndex] = inputValue;
+    setResponses(updatedResponses);
+    
+    const questionsResponses = updatedResponses.map((response, index) => {
+      const question = questions[index];
+      return { question: question.question, response };
     });
 
-    document.querySelectorAll(".questions_input").forEach((element) => {
-      element.value = "";
-    });
 
     const reponse_tab = { reponses: questionsResponses };
     sendQuestionForm(reponse_tab);
+    
+    document.querySelectorAll(".questions_input").forEach((element) => {
+      element.value = "";
+    });
   };
 
   function sendQuestionForm(questionsResponses) {
@@ -109,51 +114,63 @@ export default function Home() {
         </h1>
 
         <form onSubmit={handleSubmit}>
-          <div key={questions[currentQuestionIndex].id} className="mb-5">
-            <label
-              htmlFor={`question-${questions[currentQuestionIndex].id}`}
-              className="block mb-2 text-sm font-medium text-gray-900"
+        <div key={questions[currentQuestionIndex].id} className="mb-5">
+          <label
+            htmlFor={`question-${questions[currentQuestionIndex].id}`}
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            {questions[currentQuestionIndex].question}
+          </label>
+          <input
+            type="text"
+            id={`question-${questions[currentQuestionIndex].id}`}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+              setShowError(false); // Cacher le message d'erreur dès que l'utilisateur commence à saisir quelque chose
+            }}
+            required
+            className="questions_input block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
+          />
+          {inputValue.trim() === '' && (
+            <p className="text-red-500 text-sm mt-1">Veuillez remplir ce champ.</p>
+          )}
+        </div>
+        <div className="flex justify-between items-center mt-6">
+          {currentQuestionIndex !== 0 && (
+            <button
+              type="button"
+              className="text-sm bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-4"
+              onClick={handlePreviousQuestion}
             >
-              {questions[currentQuestionIndex].question}
-            </label>
-            <input
-              type="text"
-              id={`question-${questions[currentQuestionIndex].id}`}
-              onChange={(e) => handleChange(e)}
-              required
-              className="questions_input block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-
-          <div className="flex justify-between items-center mt-6">
-            <div className="text-gray-600">{`${currentQuestionIndex + 1} / ${
-              questions.length
-            }`}</div>
-
-            {
-            questions.length !== (currentQuestionIndex+1)? 
-            ( <button
+              Précédent
+            </button>
+          )}
+          <div className="text-gray-600">{`${currentQuestionIndex + 1} / ${questions.length}`}</div>
+          {questions.length !== currentQuestionIndex + 1 ? (
+            <button
               type="button"
               className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               onClick={handleNextQuestion}
+              disabled={inputValue.trim() === ''}
             >
               Suivant
-            </button> ) :
-            ( <button
+            </button>
+          ) : (
+            <button
               type="submit"
               className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
             >
               Envoyer
-            </button> )
-            }
-          </div>
+            </button>
+          )}
+        </div>
 
-          <div className="mt-4">
-            <div
-              className="bg-blue-500 h-2 rounded"
-              style={{ width: `${progress}%` }}
-            ></div>
-          </div>
+        <div className="mt-4">
+          <div
+            className="bg-blue-500 h-2 rounded"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
 
         </form>
       </section>
