80989e9e4c98a12aad40058fe9058f5e2d2460f2
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-12-15 09:46:22 +0100

    Update Login.js

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -54,11 +54,11 @@ export default function Login() {
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Votre identifiant</label>
                         <input type="text" name="email" id="email" onChange={handleChangeMail} value={mail} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
                     </div>
                     <div>
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Votre mot de passe</label>
                         <input type="password" name="password" id="password" onChange={handleChangePassword} value={password} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                     </div>
                   <button
